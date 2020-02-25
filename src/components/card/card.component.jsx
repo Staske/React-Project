7d9ed6a6dc6f6c5521e3ea5f431e48761a54c250
Valdas <staskevicius.valdas@gmail.com>
2020-02-25 22:14:03 +0200

    Replaced dummy images with real ones

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,7 +7,7 @@ export const Card = props => (
     <p>#{props.player.jersey_number}</p>
     <img
       alt="player"
-      src={`https://robohash.org/${props.player.jersey_number}?set=set2&size=180x180`}
+      src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${props.player.person_id}.png`}
     />
     <h2> {props.player.display_name} </h2>
     <p>{props.player.position_full}</p>
@@ -15,5 +15,10 @@ export const Card = props => (
       <b>{props.player.height_ft}</b> ft <b>{props.player.height_in}</b> in |{" "}
       <b>{props.player.weight_lbs}</b> lbs
     </p>
+    <img
+      className="logo"
+      alt="logo"
+      src="https://www.nba.com/assets/logos/teams/primary/web/MIA.svg"
+    ></img>
   </div>
 );
