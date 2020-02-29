4ed9636e0941a4e4a0a5f810dd52f038835045a4
Valdas <staskevicius.valdas@gmail.com>
2020-02-29 15:12:51 +0200

    Updates

---
+++ precache-manifest.873c84f4b76b0f147463a0986ca47366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530f04738b70e22737cf0540795b99dc",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "84c558d17b48ff2c706b",
+    "url": "/React-Project/static/css/main.1c4db0ee.chunk.css"
+  },
+  {
+    "revision": "1cbae4faf679499c2232",
+    "url": "/React-Project/static/js/2.7b422236.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Project/static/js/2.7b422236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c558d17b48ff2c706b",
+    "url": "/React-Project/static/js/main.faf7eb32.chunk.js"
+  },
+  {
+    "revision": "100e7a76fa7a8afa70f7",
+    "url": "/React-Project/static/js/runtime-main.fd2fd76c.js"
+  }
+]);
